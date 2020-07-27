9ad2df15645e454866193e81ea9d92b3eec0dcfc
furucrm-minh-hieu <hieub1609704@student.ctu.edu.vn>
2020-07-27 09:16:09 +0700

    asdsa

---
+++ resources/views/user/admin/createUser.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.admin_interface')
+
+@section('content1')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header d-flex justify-content-between align-items-end">
+                    {{ __('Create Project') }}
+
+                </div>
+
+                <div class="card-body">
+
+
+                    <form method="POST" action="">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="project_name" class="col-md-4 col-form-label text-md-right">{{ __('Project name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="project_name" type="text" class="form-control @error('project_name') is-invalid @enderror" name="project_name" value="{{ old('project_name') }}" required autocomplete="project_name" autofocus>
+
+                                @error('project_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="project-from-date" class="col-md-4 col-form-label text-md-right">{{ __('From date') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="project-from-date" type="date" class="form-control"  name="project_from_date" >
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="project-to-date" class="col-md-4 col-form-label text-md-right">{{ __('To date') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="project-to-date" type="date" class="form-control"  name="project_to_date" >
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Location') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="location_name" type="text" class="form-control @error('project_name') is-invalid @enderror" name="location_name" value="{{ old('location_name') }}" required autocomplete="project_name" autofocus>
+                                <p> lat </p>
+                                <input id="lat" name="lat" step="any" type="number" class="form-control">
+                                <p> lng </p>
+                                <input id="lng" name="lng" step="any" type="number" class="form-control">
+
+                            </div>
+                        </div>
+
+
+                         <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Create') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/welcome1', function () {
+    return view('welcome1');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home')->middleware('role');
 
-Route::get('/admin', function () {
-    return view('userAdmin/index');
-})->name('admin');
-
+Route::get('/admin2', function () {
+    return view('layouts/admin_interface');
+})->name('admin2');
+Route::get('/manager2', function () {
+    return view('layouts/manager_interface');
+})->name('manager2');
+Route::get('/worker2', function () {
+    return view('layouts/worker_interface');
+})->name('worker2');
+Route::get('/create', function () {
+    return view('page/create');
+})->name('project_create');
 
 Route::get('/manager', function () {
     return view('userManager/index');
@@ -34,3 +46,13 @@ Route::get('/manager', function () {
 Route::get('/worker', function () {
     return view('userWorker/index');
 })->name('worker');
+Route::get('/admin', 'AdminController@index');
+
+//List User
+Route::get('/listUser', function () {
+    return view('user/admin/listUser');
+})->name('listUser');
+// Create user
+Route::get('/createUser', function () {
+    return view('user/admin/createUser');
+})->name('createUser');
