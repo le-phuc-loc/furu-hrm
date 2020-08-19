02c8599a8af97dee25ea4dc3e9a735b281255e1a
furucrm-minh-hieu <hieub1609704@student.ctu.edu.vn>
2020-08-19 13:21:48 +0700

    asd

--- resources/views/role/admin/user/edit.blade.php
+++ resources/views/role/admin/user/edit.blade.php
@@ -43,11 +43,16 @@
                                 <div class="form-group row">
                                     <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>
                                     <div class="col-md-6">
-                                        <select class="custom-select" id="role" name="role" >
+                                        <select class="custom-select @error('email') is-invalid @enderror" id="role" name="role" >
                                             <option >{{$user->role}}</option>
+                                            <option value="worker">worker</option>
+                                            <option value="manager">manager</option>
                                         </select>
-
-
+                                        @error('role')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
                                     </div>
                                 </div>
                             </div>
